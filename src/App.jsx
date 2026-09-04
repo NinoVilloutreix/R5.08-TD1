8078b68c60fe87fcc681ba8f074e7ccac3c760bf
NinoVilloutreix <nino.villoutreix@etu.unilim.fr>
2026-09-04 15:15:39 +0200

    cards finies

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
+import Card from "./components/Card";
+
 function App() {
   return (
-    <body className="flex items-center justify-center h-screen">
-      <div className="flex items-center justify-center gap-2.5 shadow-2xl">
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col p-10 justify-center gap-10 shadow-2xl">
         <ul className="flex flex-col items-start justify-center">
-          <li>Room</li>
-          <li>5 people are around right now</li>
+          <li className="text-4xl font-bold">Room</li>
+          <li className="text-xl text-gray-400">5 people are around right now</li>
         </ul>
+        <div className = "cardContainer flex gap-10 flex-col justify-between">
 
-        <div className = "flex flex-col items-center justify-start">
-          <Card/>
-          <div className ="card flex gap-1">
-            <img src="" alt="" className="w-full h-full object-cover rounded-4xl"></img>
-            <ul>
-              <li>Dale Houston</li>
-              <li>Software Engineer</li>
-            </ul>
-          </div>        
+          <Card name="Dale Houston" title="Software Engineer" img="https://thispersondoesnotexist.com/random-person.jpeg"/>
+          <Card name="Madge Murphy" title="Teacher" img="https://thispersondoesnotexist.com/random-person.jpeg"/>
+          <Card name="Margaret Garner" title="Just a Nerd" img="https://thispersondoesnotexist.com/random-person.jpeg"/>
+          <Card name="Gertrude Roberts" title="Photographer" img="https://thispersondoesnotexist.com/random-person.jpeg"/>
+          <Card name="Jean Michel" title="Stone Eater" img="https://thispersondoesnotexist.com/random-person.jpeg"/>
+
+
         </div>
       </div>
-    </body>
+    </div>
 
   );
 }
