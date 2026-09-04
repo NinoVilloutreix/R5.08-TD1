aa23da65f1947a8fc51b4c7eebd3649f97b81921
NinoVilloutreix <nino.villoutreix@etu.unilim.fr>
2026-09-04 11:52:24 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-red-500">Hello, Tailwind CSS!</h1>
-    </div>
+    <body className="flex items-center justify-center h-screen">
+      <div className="flex items-center justify-center gap-2.5 shadow-2xl">
+        <ul className="flex flex-col items-start justify-center">
+          <li>Room</li>
+          <li>5 people are around right now</li>
+        </ul>
+
+        <div className = "flex flex-col items-center justify-start">
+          <Card/>
+          <div className ="card flex gap-1">
+            <img src="" alt="" className="w-full h-full object-cover rounded-4xl"></img>
+            <ul>
+              <li>Dale Houston</li>
+              <li>Software Engineer</li>
+            </ul>
+          </div>        
+        </div>
+      </div>
+    </body>
+
   );
 }
 
